1cf5d247918694889d4d6288ad71de23e34f6e4a
ngaruko <bede.ngaruko@gmail.com>
2022-02-15 17:18:42 +1300

    try hub

--- tests/scalability/run_suite.sh
+++ tests/scalability/run_suite.sh
@@ -2,55 +2,77 @@
 set -e
 sudo shutdown -P +60
 echo Cloning cht-core to /cht-core
+sudo apt install hub
+git config --global hub.protocol https
 git config --global user.name $GITHUB_ACTOR
 
-git clone --single-branch --branch $TAG_NAME https://github.com/medic/cht-core.git;
+#git clone --single-branch --branch $TAG_NAME https://github.com/medic/cht-core.git;
+hub clone medic/cht-core
+cd cht-core
+# create a topic branch
+git checkout -b jmeter-feature
+# make some changes...
+touch test-jmeter.txt
+git commit -am "done with feature"
 
-cd cht-core/tests/scalability
-export NODE_TLS_REJECT_UNAUTHORIZED=0
+# It's time to fork the repo!
+hub fork --remote-name origin
+git remote add origin git@github.com:medic/cht-core.git
 
-sudo apt-get update
+# push the changes to your new remote
+git push origin jmeter-feature
 
-echo installing JAVA
-sudo apt-get install default-jre -y
+# check the CI status for this branch
+hub ci-status --verbose
 
-echo installing node
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# open a pull request for the branch you've just pushed
+hub pull-request
 
-echo "Changing config to match url arg"
-node -p "const fs = require('fs');var path = './config.json';var config = JSON.stringify({...require(path), url: '$MEDIC_URL/medic'}, null, 2);fs.writeFileSync(path,config,{encoding:'utf8',flag:'w'});"
-echo "npm install for jmeter suite"
-npm install
-echo "jmeter install"
-wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.4.3.tgz -O ./apache-jmeter.tgz &&
-mkdir ./jmeter && tar -xf apache-jmeter.tgz -C ./jmeter --strip-components=1
-echo "Installing Plugins" &&
-wget  https://repo1.maven.org/maven2/kg/apc/jmeter-plugins-manager/1.4/jmeter-plugins-manager-1.4.jar -O ./jmeter/lib/ext/jmeter-plugins-manager-1.4.jar &&
-wget 'http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.2/cmdrunner-2.2.jar' -O ./jmeter/lib/cmdrunner-2.2.jar &&
-java -cp jmeter/lib/ext/jmeter-plugins-manager-1.4.jar org.jmeterplugins.repository.PluginManagerCMDInstaller &&
-./jmeter/bin/PluginsManagerCMD.sh install jpgc-mergeresults &&
-echo "jmeter do it!"
-./jmeter/bin/jmeter -n  -t sync.jmx -Jworking_dir=./ -Jnode_binary=$(which node) -l ./report/cli_run.jtl -e -o ./report
-mv ./jmeter.log ./report/jmeter.log
-echo "Installing AWS CLI"
-sudo apt-get install unzip -y
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-# echo "Uploading logs and screenshots to ${S3_PATH}..."
-# /usr/local/bin/aws s3 cp ./report "$S3_PATH" --recursive
-git switch -c jmeter-test3-${TAG_NAME}
-mv report/cli_run.jtl previous_results/${TAG_NAME}.jtl
+# cd cht-core/tests/scalability
+# export NODE_TLS_REJECT_UNAUTHORIZED=0
 
-git add previous_results/*
-git commit -m'Adding jmeter restults'
-zip -r report.zip report
-git add report.zip
-git commit -m'Adding zip report'
-#git restore .
+# sudo apt-get update
+
+# echo installing JAVA
+# sudo apt-get install default-jre -y
+
+# echo installing node
+# curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+# sudo apt-get install -y nodejs
+
+# echo "Changing config to match url arg"
+# node -p "const fs = require('fs');var path = './config.json';var config = JSON.stringify({...require(path), url: '$MEDIC_URL/medic'}, null, 2);fs.writeFileSync(path,config,{encoding:'utf8',flag:'w'});"
+# echo "npm install for jmeter suite"
+# npm install
+# echo "jmeter install"
+# wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.4.3.tgz -O ./apache-jmeter.tgz &&
+# mkdir ./jmeter && tar -xf apache-jmeter.tgz -C ./jmeter --strip-components=1
+# echo "Installing Plugins" &&
+# wget  https://repo1.maven.org/maven2/kg/apc/jmeter-plugins-manager/1.4/jmeter-plugins-manager-1.4.jar -O ./jmeter/lib/ext/jmeter-plugins-manager-1.4.jar &&
+# wget 'http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.2/cmdrunner-2.2.jar' -O ./jmeter/lib/cmdrunner-2.2.jar &&
+# java -cp jmeter/lib/ext/jmeter-plugins-manager-1.4.jar org.jmeterplugins.repository.PluginManagerCMDInstaller &&
+# ./jmeter/bin/PluginsManagerCMD.sh install jpgc-mergeresults &&
+# echo "jmeter do it!"
+# ./jmeter/bin/jmeter -n  -t sync.jmx -Jworking_dir=./ -Jnode_binary=$(which node) -l ./report/cli_run.jtl -e -o ./report
+# mv ./jmeter.log ./report/jmeter.log
+# echo "Installing AWS CLI"
+# sudo apt-get install unzip -y
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install
+# # echo "Uploading logs and screenshots to ${S3_PATH}..."
+# # /usr/local/bin/aws s3 cp ./report "$S3_PATH" --recursive
+# git switch -c jmeter-test3-${TAG_NAME}
+# mv report/cli_run.jtl previous_results/${TAG_NAME}.jtl
+
+# git add previous_results/*
+# git commit -m'Adding jmeter restults'
+# zip -r report.zip report
+# git add report.zip
+# git commit -m'Adding zip report'
+# #git restore .
 
 
-git push --set-upstream origin jmeter-test3-${TAG_NAME}
-git request-pull master origin
+# git push --set-upstream origin jmeter-test3-${TAG_NAME}
+# git request-pull master origin
 echo "FINISHED! "
